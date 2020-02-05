4526993989c6a24ec1778c89098a6e92be6456b2
whichxjy <whichxjy@gmail.com>
2020-02-06 04:27:59 +0800

    Update snakeCharmer.js

--- composition/snakeCharmer.js
+++ composition/snakeCharmer.js
@@ -1,14 +1,28 @@
-'use strict';
+'use strict'
 
-const pipeline = require('./pipeline.js');
-const { count, head, tail } = require('../arrayHelpers.js');
-const { map, reduce, filter } = require('../higher-order/filterMapReduce.js');
+const { pipeline } = require('./pipeline.js')
+const { count, head, tail } = require('../arrayHelpers.js')
+const { map } = require('../higher-order/filterMapReduce.js')
 
 // TODO Define reusable, single-argument functions for pipelining here
 
+const splitByUnderscore = (str) => str.split('_')
 
-const snakeToCamel = pipeline( ); //TODO
-const snakeToTrain = pipeline( ); //TODO
+const capitalize = (str) => str[0].toUpperCase() + str.slice(1)
+const capitalizeAll = (strArr) => map(capitalize, strArr)
+const capitalizeTail = (strArr) => {
+  if (count(strArr) === 0) {
+    return []
+  } else {
+    return [head(strArr)].concat(capitalizeAll(tail(strArr)))
+  }
+}
 
+const createJoiner = (separator) => (strArr) => strArr.join(separator)
+const joinWithNothing = createJoiner('')
+const joinWithHyphen = createJoiner('-')
 
-module.exports = { snakeToCamel, snakeToTrain };
+const snakeToCamel = pipeline(splitByUnderscore, capitalizeTail, joinWithNothing)
+const snakeToTrain = pipeline(splitByUnderscore, capitalizeAll, joinWithHyphen)
+
+module.exports = { snakeToCamel, snakeToTrain }
