fc5c129953612bab28aac54239695daf3294ad18
whichxjy <whichxjy@gmail.com>
2020-02-06 02:08:00 +0800

    Update functionalShapes.js

--- closure/functionalShapes.js
+++ closure/functionalShapes.js
@@ -1,26 +1,27 @@
-'use strict';
+'use strict'
 
 const rectangle = (height, width) => {
   return (method) => {
     switch (method) {
-      case 'getWidth': return () => width;
-      case 'getHeight': return  //TODO
-      case 'getArea': return //TODO
-      case 'setWidth': return (newWidth) => rectangle(height, newWidth);
-      case 'setHeight': return  //TODO
-      case 'toString': return () => new Array(height+1).join(new Array(width+1).join('X ') + '\n');
-      default: return (...args) => `unknown method "${method}"`;
+      case 'getWidth': return () => width
+      case 'getHeight': return () => height
+      case 'getArea': return () => width * height
+      case 'setWidth': return (newWidth) => rectangle(height, newWidth)
+      case 'setHeight': return (newHeight) => rectangle(newHeight, width)
+      case 'toString': return () => new Array(height + 1).join(new Array(width + 1).join('X ') + '\n')
+      default: return () => `unknown method "${method}"`
     }
   }
-};
+}
 
 const square = (side) => {
   return (method) => {
     if (method === 'setSize' || method === 'setWidth' || method === 'setHeight') {
-      return (size) => // TODO
+      return (size) => rectangle(size, size)
+    } else {
+      return rectangle(side, side)(method)
     }
-    else return //TODO
   }
-};
+}
 
-module.exports = { rectangle, square };
+module.exports = { rectangle, square }
