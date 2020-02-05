fd991fa0bfad9ce7b7b63310244d8684e00580e7
whichxjy <whichxjy@gmail.com>
2020-02-06 01:56:10 +0800

    Update grumpyQuotesCurried.js

--- closure/grumpyQuotesCurried.js
+++ closure/grumpyQuotesCurried.js
@@ -1,16 +1,12 @@
-'use strict';
+'use strict'
 
+const curriedQuote = (name) => (year) => (text) => `"${text}"\n\t- ${name} (${year})\n\n`
 
-const curriedQuote =  //TODO
-
-
-const quoteCrockford = curriedQuote('Doug Crockford');
-const quoteDijkstra = curriedQuote('Edsger Dijkstra');
-const quoteDijkstra75 = quoteDijkstra(1975);
-
+const quoteCrockford = curriedQuote('Doug Crockford')
+const quoteDijkstra = curriedQuote('Edsger Dijkstra')
+const quoteDijkstra75 = quoteDijkstra(1975)
 
 const grumpyQuotes = [
-
   curriedQuote('Guido van Rossum')(2001)("Don't you hate code that's not properly indented?"), // https://slashdot.org/story/01/04/20/1455252/guido-van-rossum-unleashed
 
   quoteCrockford(2005)('There are no good texts on JavaScript programming.'), // http://crockford.com/javascript/style1.html
@@ -20,12 +16,11 @@ const grumpyQuotes = [
   quoteDijkstra75('By claiming that they can contribute to software engineering, the soft scientists make themselves even more ridiculous.'), // https://www.cs.utexas.edu/users/EWD/ewd04xx/EWD498.PDF
   quoteDijkstra75("Besides a mathematical inclination, an exceptionally good mastery of one's native tongue is the most vital asset of a competent programmer."), // ibid.
   quoteDijkstra75("The irony of my self-contradiction is lost on me. [Just kidding, he didn't actually say this one - making it all the more true!]") // ibid.
-
-];
+]
 
 if (require.main === module) {
-  const { map } = require('../higher-order/filterMapReduce.js');
+  const { map } = require('../higher-order/filterMapReduce.js')
 
   // Warning: non-functional console.logging ahead!
-  map(console.log, grumpyQuotes);
+  map(console.log, grumpyQuotes)
 }
