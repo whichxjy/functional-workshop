e3ddb4f1e363453103587898004c5d9592d28f3d
whichxjy <whichxjy@gmail.com>
2020-02-06 06:10:24 +0800

    Update compareSums.js

--- immutability/compareSums.js
+++ immutability/compareSums.js
@@ -1,21 +1,20 @@
-'use strict';
-
+'use strict'
 
 const iterativeSum = (numArray) => {
-  let total = 0;
-  for (let value of numArray) {
-    total += value;
+  let total = 0
+  for (const value of numArray) {
+    total += value
   }
-  return total;
-};
-
-const reduceSum = (numArray) => {
-  return numArray.reduce((acc, val) => acc + val);
-};
-
-const moriSum = (numVector) => {
-  const { reduce, sum } = require('mori');
-  return reduce(sum, 0, numVector);
+  return total
 }
 
-module.exports = { iterativeSum, reduceSum, moriSum };
+const reduceSum = (numArray) => {
+  return numArray.reduce((acc, val) => acc + val)
+}
+
+const moriSum = (numVector) => {
+  const { reduce, sum } = require('mori')
+  return reduce(sum, 0, numVector)
+}
+
+module.exports = { iterativeSum, reduceSum, moriSum }
