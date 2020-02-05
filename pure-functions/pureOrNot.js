54778cd18ae1e013cda2854f6b7a1b6a143b7fc5
whichxjy <whichxjy@gmail.com>
2020-02-05 21:37:05 +0800

    Update pureOrNot.js

--- pure-functions/pureOrNot.js
+++ pure-functions/pureOrNot.js
@@ -1,72 +1,60 @@
-'use strict';
+'use strict'
 
+/// /////////////
 
-////////////////
+const getDate = () => new Date().toDateString()
 
+const getWorkshopDate = () => new Date(2018, 3, 28).toDateString()
 
-const getDate = () => new Date().toDateString();
-
-const getWorkshopDate = () => new Date(2018, 3, 28).toDateString();
-
-
-
-////////////////
-
-
+/// /////////////
 
 const jsonToObj = (filename) => {
-  const fs = require('fs');
-  const fileData = fs.readFileSync(filename, 'utf8');
-  return JSON.parse(fileData);
+  const fs = require('fs')
+  const fileData = fs.readFileSync(filename, 'utf8')
+  return JSON.parse(fileData)
 }
 
 const objToJson = (object) => {
-  return JSON.stringify(object, null, 2);
+  return JSON.stringify(object, null, 2)
 }
 
+/// /////////////
 
-
-////////////////
-
-
-const logicalXor = (A, B) => (A || B) && !(A && B);
+const logicalXor = (A, B) => (A || B) && !(A && B)
 
 const truthTable = (logicalOperator) => {
-  const values = [true, false];
-  const table = [];
+  const values = [true, false]
+  const table = []
   for (const A of values) {
     for (const B of values) {
-      table.push([A, B, logicalOperator(A, B)]);
+      table.push([A, B, logicalOperator(A, B)])
     }
   }
-  return table;
-};
+  return table
+}
 
 const xorTruthTable = () => {
-  console.log('XOR truth table:\n================\n');
-  const table = truthTable(logicalXor);
-  console.log('\tA\t|\tB\t|\tA xor B');
-  console.log('\t-----\t|\t-----\t|\t-----');
-  table.map(([A, B, result]) => console.log(`\t${A}\t|\t${B}\t|\t${logicalXor(A,B)}`));
-};
+  console.log('XOR truth table:\n================\n')
+  const table = truthTable(logicalXor)
+  console.log('\tA\t|\tB\t|\tA xor B')
+  console.log('\t-----\t|\t-----\t|\t-----')
+  table.map(([A, B, result]) => console.log(`\t${A}\t|\t${B}\t|\t${logicalXor(A, B)}`))
+}
 
-
-
-
-////////////////
+/// /////////////
 
 // function examples in this section taken from
 // https://github.com/mapbox/mapbox-sdk-js/blob/master/lib/helpers/url-utils.js
 
-function encodeArray(arrayValue) {
-  return arrayValue.map(encodeURIComponent).join(',');
+function encodeArray (arrayValue) {
+  return arrayValue.map(encodeURIComponent).join(',')
 }
 
-function encodeValue(value) {
+function encodeValue (value) {
   if (Array.isArray(value)) {
-    return encodeArray(value);
+    return encodeArray(value)
   }
-  return encodeURIComponent(String(value));
+  return encodeURIComponent(String(value))
 }
 
 /**
@@ -79,26 +67,24 @@ function encodeValue(value) {
  *   preserved, unencoded.
  * @returns {string} - Modified URL.
  */
-function appendQueryParam(url, key, value) {
+function appendQueryParam (url, key, value) {
   if (value === false || value === null) {
-    return url;
+    return url
   }
-  var punctuation = /\?/.test(url) ? '&' : '?';
-  var query = encodeURIComponent(key);
+  var punctuation = /\?/.test(url) ? '&' : '?'
+  var query = encodeURIComponent(key)
   if (value !== undefined && value !== '' && value !== true) {
-    query += '=' + encodeValue(value);
+    query += '=' + encodeValue(value)
   }
-  return '' + url + punctuation + query;
+  return '' + url + punctuation + query
 }
 
-
-////////////////
-
+/// /////////////
 
 const sum = (numArray) => {
-  let total = 0;
-  for (let value of numArray) {
-    total += value;
+  let total = 0
+  for (const value of numArray) {
+    total += value
   }
-  return total;
-};
+  return total
+}
