9d96b4a8230263dab7435cfc5c8bc2ab3892d964
whichxjy <whichxjy@gmail.com>
2020-02-06 04:03:29 +0800

    Update pipeline.js

--- composition/pipeline.js
+++ composition/pipeline.js
@@ -1,15 +1,20 @@
-'use strict';
+'use strict'
 
-const { count, head, tail } = require('../arrayHelpers.js');
-const { reduce } = require('../higher-order/filterMapReduce.js');
+const { count, head, tail } = require('../arrayHelpers.js')
+const { reduce } = require('../higher-order/filterMapReduce.js')
 
 const pipeline = (...functions) => {
-  // TODO
-};
+  if (count(functions) === 0) {
+    return (input) => input
+  } else if (count(functions) === 1) {
+    return (input) => head(functions)(input)
+  } else {
+    return (input) => pipeline(...tail(functions))(head(functions)(input))
+  }
+}
 
 const reducePipeline = (...functions) => {
-  // TODO
-};
+  return (input) => reduce((acc, func) => func(acc), input, functions)
+}
 
-
-module.exports = pipeline;
+module.exports = { pipeline, reducePipeline }
