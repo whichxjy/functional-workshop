18edacadf5c96ee7c6d5a398aaac31b1929108bd
whichxjy <whichxjy@gmail.com>
2020-02-05 21:37:44 +0800

    Finish impureToPure exercise

--- pure-functions/impureToPure.js
+++ pure-functions/impureToPure.js
@@ -1,19 +1,27 @@
-'use strict';
+'use strict'
 
 // TODO Copy the impure version of your function here:
 
+let Multiplier = 2
 
+function mul (x, y) {
+  return Multiplier * x * y
+}
 
+// Test
 
+console.log(mul(2, 3))
 
+Multiplier = 4
 
-
-
-
-
-
-
-
-
+console.log(mul(2, 3))
 
 // TODO Now write a refactored, pure version of the function:
+
+function pureMul (x, y) {
+  return 2 * x * y
+}
+
+// Test
+
+console.log(pureMul(2, 3))
