8348c8e9406e50e6c557998380bcb2a26674329c
whichxjy <whichxjy@gmail.com>
2020-02-06 01:00:36 +0800

    Update arrayHelpers.js

--- arrayHelpers.js
+++ arrayHelpers.js
@@ -1,19 +1,17 @@
-'use strict';
-
+'use strict'
 
 // HELPER FUNCTIONS to get used to a more functional style
 // Use these (e.g. count(array) instead of array.length, etc.)
 
 // NOTE: The functions in this module are for educational purposes, not high-performance production use!
 
-
 // get the number of elements in an array
-const count = (array) => array.length;
+const count = (array) => array.length
 
 // get the first element of an array
-const head = (array) => array[0];
+const head = (array) => array[0]
 
 // get the rest of the array (all except the first element)
-const tail = (array) => array.slice(1);
+const tail = (array) => array.slice(1)
 
-module.exports = { count, head, tail };
+module.exports = { count, head, tail }
