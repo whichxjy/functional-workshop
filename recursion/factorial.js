22acb7303366ba1445a8a1c27e9ea066b14fadfb
whichxjy <whichxjy@gmail.com>
2020-02-05 21:38:33 +0800

    Finish factorial exercise

--- recursion/factorial.js
+++ recursion/factorial.js
@@ -1,19 +1,39 @@
-'use strict';
-
+'use strict'
 
 const imperativeFactorial = (n) => {
-  // TODO
-
-};
-
+  let result = 1
+  for (let i = 2; i <= n; i++) {
+    result *= i
+  }
+  return result
+}
 
 const recursiveFactorial = (n) => {
-  // TODO
+  if (n <= 1) {
+    return 1
+  } else {
+    return n * recursiveFactorial(n - 1)
+  }
+}
 
-};
+const tailRecursiveFactorial = (n, result = 1) => {
+  if (n <= 1) {
+    return result
+  } else {
+    return tailRecursiveFactorial(n - 1, result * n)
+  }
+}
 
+// Test
 
-const tailRecursiveFactorial = (n) => {
-  // TODO
+console.assert(imperativeFactorial(0) === 1)
+console.assert(recursiveFactorial(0) === 1)
+console.assert(tailRecursiveFactorial(0) === 1)
 
-};
+console.assert(imperativeFactorial(1) === 1)
+console.assert(recursiveFactorial(1) === 1)
+console.assert(tailRecursiveFactorial(1) === 1)
+
+console.assert(imperativeFactorial(8) === 40320)
+console.assert(recursiveFactorial(8) === 40320)
+console.assert(tailRecursiveFactorial(8) === 40320)
